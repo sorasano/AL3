9ec9c4f363e6d225492de580fb9a5d014b4570a2
SoraSano <k021g1331@g.neec.ac.jp>
2022-04-14 15:59:50 +0900

    デバック表示の修正

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -38,13 +38,13 @@ void GameScene::Update() {
 
 	//書式指定付き表示
 	debugText_->SetPos(50,70);
-	debugText_->Printf("translation:(%f,%f,%f)", 10.0f, 10.0f, 10.0f);
+	debugText_->Printf("translation:(%f,%f,%f)", worldTransform_.translation_.x, worldTransform_.translation_.y,worldTransform_.translation_.z);
 
 	debugText_->SetPos(50, 90);
-	debugText_->Printf("rotation:(%f,%f,%f)", XM_PI / 4.0f, XM_PI / 4.0f, 0.0f);
+	debugText_->Printf("rotation:(%f,%f,%f)", worldTransform_.rotation_.x, worldTransform_.rotation_.y,worldTransform_.rotation_.z);
 	
 	debugText_->SetPos(50, 110);
-	debugText_->Printf("scale_:(%f,%f,%f)", 5.0f, 5.0f, 5.0f);
+	debugText_->Printf("scale_:(%f,%f,%f)", worldTransform_.scale_.x, worldTransform_.scale_.y, worldTransform_.scale_.z);
 }
 
 void GameScene::Draw() {
