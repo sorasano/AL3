fca00b0ce350ab303d44010b159aef6709162a15
SoraSano <k021g1331@g.neec.ac.jp>
2022-04-14 13:50:44 +0900

    ウィンドウのタイトルバーの変更

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow("LE2B_11_サノ_ソラ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
